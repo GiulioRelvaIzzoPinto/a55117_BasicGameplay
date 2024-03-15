0541ffd15943f760755758a21b19ea88ccc06a69
Giulio <giuliorelvaip@gmail.com>
2024-03-15 11:33:57 +0000

    (2)4.
    Move the player left-to-right

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float speed = 10.0f;
     void Start()
     {
         
@@ -12,5 +13,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontalInput =  Input.GetAxis(“Horizontal”);
+        transform.Translate (Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
