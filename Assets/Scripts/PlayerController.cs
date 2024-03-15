c7558e9d35db0dd232129dc785007b105822a783
Giulio <giuliorelvaip@gmail.com>
2024-03-15 12:54:58 +0000

    (2)6.
    Clean up your code and variables

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,15 +6,20 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
+    public float xRange = 10;
     void Start()
     {
         
     }
     void Update()
     {
-        if (transform.position.x < -10)
+        if (transform.position.x < /*-10*/ - xRange)
         {
-            transform.position = new Vector3 (-10, transform.position.y, transform.position.z);
+            transform.position = new Vector3 (/*-10*/ - xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3 (xRange, transform.position.y, transform.position.z);
         }
             horizontalInput =  Input.GetAxis("Horizontal");
             transform.Translate (Vector3.right * horizontalInput * Time.deltaTime * speed);
