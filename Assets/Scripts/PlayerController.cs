5bcca42ef7ae17c10a0602c19c5df6832cfd556d
Giulio <giuliorelvaip@gmail.com>
2024-03-15 11:31:54 +0000

    (2)3
    Get the user’s horizontal input

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        horizontalInput =  Input.GetAxis(“Horizontal”);
+    }
+}
